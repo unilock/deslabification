aa14cc2cfb3c78292be9ae28d017fac1b8bfdb52
unilock <unilock@fennet.rentals>
2024-07-14 14:14:56 -0400

    simplify logic slightly
    Signed-off-by: unilock <unilock@fennet.rentals>

--- xplat/src/main/java/cc/unilock/deslabification/Deslabification.java
+++ xplat/src/main/java/cc/unilock/deslabification/Deslabification.java
@@ -15,32 +15,30 @@ public class Deslabification {
     public static final String MOD_ID = "deslabification";
 
     public static void process(Map<RecipeType<?>, ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>>> map, ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>> builder, RecipeHolder<?> holder) {
-        if (holder.value() instanceof ShapedRecipe recipe && recipe.getResultItem(null).getCount() == 6) {
+        if (holder.value() instanceof ShapedRecipe recipe && recipe.getResultItem(null).getItem() instanceof BlockItem bi && bi.getBlock() instanceof SlabBlock slab && recipe.getResultItem(null).getCount() == 6) {
             List<Ingredient> ingredients = recipe.getIngredients().stream().filter(i -> !i.isEmpty()).toList();
             if (ingredients.size() == 3 && ingredients.stream().allMatch(ingredients.get(0)::equals)) {
-                if (recipe.getResultItem(null).getItem() instanceof BlockItem bi && bi.getBlock() instanceof SlabBlock slab) {
-                    ItemStack input = ingredients.get(0).getItems()[0];
+                ItemStack input = ingredients.get(0).getItems()[0];
 
-                    ResourceLocation blockId = BuiltInRegistries.ITEM.getKey(input.getItem());
-                    ResourceLocation slabId = BuiltInRegistries.BLOCK.getKey(slab);
-                    ResourceLocation recipeId = new ResourceLocation(MOD_ID, blockId.getNamespace()+"_"+blockId.getPath() + "/" +slabId.getNamespace()+"_"+slabId.getPath());
+                ResourceLocation blockId = BuiltInRegistries.ITEM.getKey(input.getItem());
+                ResourceLocation slabId = BuiltInRegistries.BLOCK.getKey(slab);
+                ResourceLocation recipeId = new ResourceLocation(MOD_ID, blockId.getNamespace()+"_"+blockId.getPath() + "/" +slabId.getNamespace()+"_"+slabId.getPath());
 
-                    RecipeHolder<ShapedRecipe> recipeHolder = new RecipeHolder<>(recipeId, new ShapedRecipe(
-                            "",
-                            CraftingBookCategory.BUILDING,
-                            ShapedRecipePattern.of(
-                                    Map.of(
-                                            '#',
-                                            Ingredient.of(slab)
-                                    ),
-                                    "##"
-                            ),
-                            input
-                    ));
+                RecipeHolder<ShapedRecipe> recipeHolder = new RecipeHolder<>(recipeId, new ShapedRecipe(
+                        "",
+                        CraftingBookCategory.BUILDING,
+                        ShapedRecipePattern.of(
+                                Map.of(
+                                        '#',
+                                        Ingredient.of(slab)
+                                ),
+                                "##"
+                        ),
+                        input
+                ));
 
-                    map.computeIfAbsent(RecipeType.CRAFTING, p -> ImmutableMap.builder()).put(recipeId, recipeHolder);
-                    builder.put(recipeId, recipeHolder);
-                }
+                map.computeIfAbsent(RecipeType.CRAFTING, p -> ImmutableMap.builder()).put(recipeId, recipeHolder);
+                builder.put(recipeId, recipeHolder);
             }
         }
     }
